ba551fd758e52ec4372092ed2c3256d46a2f1790
Ben Lackey <ben.lackey@outlook.com>
2018-10-16 19:25:40 -0700

    and.... I'm stuck

--- scripts/imdg.sh
+++ scripts/imdg.sh
@@ -18,20 +18,9 @@ cd /opt
 curl -O https://download.hazelcast.com/enterprise/hazelcast-enterprise-3.10.6.zip
 unzip hazelcast-enterprise-3.10.6.zip
 cd hazelcast-enterprise-3.10.6
+cd bin
 
-#####
+# This isn't going to work because there isn't a license.
 
-rallyDNS="couchbase-server.hazelcast.hazelcast.oraclevcn.com"
-nodeDNS=$(hostname)
-nodeDNS+=".hazelcast.hazelcast.oraclevcn.com"
-
-echo "Using the settings:"
-echo rallyDNS \'$rallyDNS\'
-echo nodeDNS \'$nodeDNS\'
-
-if [[ $rallyDNS == $nodeDNS ]]
-then
-  echo "I'm the rally point."
-else
-  echo "I'm just a regular node."
-fi
+# This is not a real solution:
+nohup ./start.sh &
