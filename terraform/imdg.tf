4fd48cc26148bc5d131a5b13891d336ff4fa5ca3
Oguz Pastirmaci <oguzp@outlook.com>
2019-04-15 15:48:34 -0700

    Update templates

--- terraform/imdg.tf
+++ terraform/imdg.tf
@@ -1,37 +1,28 @@
-resource "oci_core_instance" "imdg" {
-  display_name        = "imdg"
+data "template_file" "hazelcast" {
+  template = "${file("../scripts/imdg.sh")}"
+}
+
+resource "oci_core_instance" "Hazelcast" {
+  count               = "${var.NumInstances}"
+  availability_domain = "${lookup(data.oci_identity_availability_domains.ADs.availability_domains[var.availability_domain - 1],"name")}"
   compartment_id      = "${var.compartment_ocid}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
-  shape               = "${var.imdg["shape"]}"
-  subnet_id           = "${oci_core_subnet.subnet.id}"
+  display_name        = "Hazelcast-IMDG-${count.index}"
+  shape               = "${var.instance_shape}"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.HazelcastSubnet.id}"
+    display_name     = "primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "hazelcastimdg-${count.index}"
+  }
 
   source_details {
-    source_id   = "${var.images[var.region]}"
     source_type = "image"
+    source_id   = "${var.images[var.region]}"
   }
 
   metadata {
     ssh_authorized_keys = "${var.ssh_public_key}"
-
-    user_data = "${base64encode(format("%s\n%s\n",
-      "#!/usr/bin/env bash",
-      file("../scripts/imdg.sh")
-    ))}"
+    user_data           = "${base64encode(data.template_file.hazelcast.rendered)}"
   }
-
-  count = "${var.imdg["node_count"]}"
-}
-
-data "oci_core_vnic_attachments" "imdg_vnic_attachments" {
-  compartment_id      = "${var.compartment_ocid}"
-  availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
-  instance_id         = "${oci_core_instance.imdg.*.id[0]}"
-}
-
-data "oci_core_vnic" "imdg_vnic" {
-  vnic_id = "${lookup(data.oci_core_vnic_attachments.imdg_vnic_attachments.vnic_attachments[0],"vnic_id")}"
-}
-
-output "IMDG URL" {
-  value = "http://${data.oci_core_vnic.imdg_vnic.public_ip_address}:5701"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,59 +1,36 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# Environmental variables
-# You probably want to define these as environmental variables.
-# Instructions on that are here: https://github.com/cloud-partners/oci-prerequisites
-# ---------------------------------------------------------------------------------------------------------------------
-
-# Required by the OCI Provider
 variable "tenancy_ocid" {}
-
-variable "compartment_ocid" {}
 variable "user_ocid" {}
 variable "fingerprint" {}
 variable "private_key_path" {}
 variable "region" {}
 
-# Key used to SSH to OCI VMs
+variable "compartment_ocid" {}
 variable "ssh_public_key" {}
 
-# ---------------------------------------------------------------------------------------------------------------------
-# Optional variables
-# The defaults here will give you a cluster.  You can also modify these.
-# ---------------------------------------------------------------------------------------------------------------------
-
-variable "imdg" {
-  type = "map"
-
-  default = {
-    shape      = "VM.Standard2.4"
-    node_count = 3
-  }
+# Defines the number of instances to deploy
+variable "NumInstances" {
+  default = "1"
 }
 
-variable "jet" {
-  type = "map"
-
-  default = {
-    shape      = "VM.Standard2.4"
-    node_count = 2
-  }
+# Choose an Availability Domain
+variable "availability_domain" {
+  default = "1"
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# Constants
-# You probably don't need to change these.
-# ---------------------------------------------------------------------------------------------------------------------
+variable "instance_shape" {
+  default = "VM.Standard2.2"
+}
 
 // https://docs.cloud.oracle.com/iaas/images/image/cf34ce27-e82d-4c1a-93e6-e55103f90164/
-// Oracle-Linux-7.6-2019.03.22-0
+// Oracle-Linux-7.6-2019.03.22-1
 variable "images" {
   type = "map"
 
   default = {
-    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaandary2dpwhw42xgv2d3zsbax2hln4wgcrm2tulo3dg67mwkly6aq"
-    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaavzrrzlq2zvj5fd5c27jed7fwou5aqkezxbtmys4aolls54zg7f7q"
-    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaacvcy3avanrdb4ida456dgktfhab2phyaikmw75yflugq37eu6oya"
-    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaajsuyctwgcvgfkqar2m7znxj25oqwkb7a7tucnrp2adbzoajasspq"
-    ca-toronto-1   = "ocid1.image.oc1.ca-toronto-1.aaaaaaaalbg6mthxa6jjmwxb2477px4xb3azu4fl7kubp54s4rrvtswqmo6q"
+    eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaa2n5z4nmkqjf27btkbdibflwvximz5i3rsz57c3gowckozrdshnua"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaavxqdkuyamlnrdo3q7qa7q3tsd6vnyrxjy3nmdbpv7fs7um53zh5q"
+    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaapxvrtwbpgy3lchk2usn462ekarljwg4zou2acmundxlkzdty4bjq"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaaaxnnrqke453ur5katouvfn2i6oweuwpixx6mm5e4nqtci7oztx5a"
+    ca-toronto-1   = "ocid1.image.oc1.ca-toronto-1.aaaaaaaaqopv4wgbh54jrqoa4bjpkng2y2npzoe2jaj5pdne37ljdxbbbdka"
   }
 }
